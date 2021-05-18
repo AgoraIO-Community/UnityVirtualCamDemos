79a44b05b1751cca449d18bcbe6324810577c974
Rick Cheng <rick@agora.io>
2021-05-17 22:43:19 -0700

    Simple and AR scene for using the VirtualCamPrefab

---
+++ Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    TextMesh textMesh;
+    [SerializeField]
+    float PingPongLength = 1;
+
+    MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMesh>();
+        if (textMesh == null)
+        {
+            meshRenderer = GetComponent<MeshRenderer>();	
+	    }
+    }
+
+
+    void Update()
+    {
+        if (textMesh != null)
+        {
+            textMesh.color = Color.Lerp(Color.blue, Color.red, Mathf.PingPong(Time.time, PingPongLength));
+        } else
+        if (meshRenderer != null)
+        { 
+	        meshRenderer.material.color = Color.Lerp(Color.green, Color.red, Mathf.PingPong(Time.time, PingPongLength));
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScreenScaler.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ScreenScaler : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        float quadHeight = Camera.main.orthographicSize * 2;
+        float quadWidth = quadHeight * Screen.width / Screen.height;
+        transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+    }
+}
